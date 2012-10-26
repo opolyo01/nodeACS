c3b0c416a4049056e7992c973be747405cf44bd5
Oleg Polyakov <opolyo01@yahoo.com>
2012-10-25 22:57:47 -0700

    Added some extra use cases for node acs testing

--- myapp.js
+++ myapp.js
@@ -1,20 +1,4 @@
-/**
- * service definition file
- */
-// logger.setLevel('DEBUG');
-// 
-// api.index = function(req, res) {
-// 	res.text('Index');
-// };
-// 
-// api.edit = function(req, res) {
-// 	res.text('Edit');
-// };
-// api.getJson = function(req, res) {
-//     res.json({ success: true, message: "Hello, World!" });
-// };
-
-ACS.init('5c28c2cbk18dak4cd7kb1dck24ef436961ac', 'cec12b6cs73f0s4073sbd4as6002fe3ed65d');
+ACS.init('mrkzNrohgOs6cqPx3QWglxoLpm7DuV4r', '1Tbd1X2iqxlhcJZgw9ZSV5e6lE9Vnouk');
 
 logger.setLevel('DEBUG');
 
@@ -39,6 +23,7 @@ api.create = function(req, res) {
 	            'id: ' + user.id + '\\n' +
 	            'first name: ' + user.first_name + '\\n' +
 	            'last name: ' + user.last_name);
+			res.end();
 	    } else {
 	        logger.info('Error:\\n' +
 	            ((e.error && e.message) || JSON.stringify(e)));
@@ -63,12 +48,11 @@ api.createPlace = function(req, res) {
     res.write('Creating a new place...\n');
   
     ACS.Places.create({
-      name:'appc_beijing', 
-      city:'Beijing'
+      name:'Google', 
+      city:'Mountain View',
+	  state: 'CA'
     }, function(data) {
         res.write(JSON.stringify(data, null, 2));
-        res.write('\nAppcelerator Beijing office Created!!\n');
         res.end();
-        logger.info('Appcelerator Beijing office Created!!');
     }, req, res);
 };
