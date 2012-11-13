9c771bcf5071e47e8a768f735b56b9b770976c87
Oleg Polyakov <opolyo01@yahoo.com>
2012-11-12 23:51:29 -0800

    node acs project checkin create/login

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Project README file
+Node.ACS is pretty exciting framework for getting your node js applications to talk to the Cloud.  I started step by step with documentation on
+https://cloud.appcelerator.com/docs/nodeacs/custom_code_tutorial#quickstart but got stuck on the step where I needed to authenticate before
+creating users and places.
+Here's gotcha #1:
+
+- Use the proper key/secret in ACS.init('my_oauth_key', 'my_oauth_secret');  Note that there's both development and production pairs.
+I spend more than hour, and pinging Arthur Evans before realizing it.
+
+Gotcha #2
+
+- You can call ACS related functions only in the scope api.  I tried invoking ACS.Places.create from private function, but it didn't work
+
+I posted my hack of today into https://github.com/opolyo01/nodeACS 
---
+++ app.js
@@ -0,0 +1,75 @@
+var sessionId,
+	util = require('util'),
+	qs = require('querystring');
+	
+
+logger.setLevel('DEBUG');
+
+api.login = function(req, res, callback) {
+	var query = req.query,
+		email = query.email,
+		password = query.password;
+	ACS.Users.login({login: email, password: password}, loginCallback);
+	function loginCallback(data) {
+		var response = {status: "failure"};
+		if(data.meta && data.meta.session_id){
+			response = {status: "success"};
+		}
+		res.send(response);
+	}
+};
+
+api.registerSubmit = function(req, res, callback) {
+	var query = req.query,
+		email = query.email,
+		firstName = query.firstName,
+		lastName = query.lastName,
+		password = query.password,
+		passwordConf = query.passwordConf;
+		ACS.Users.create({
+		    email: email,
+			first_name: firstName,
+			last_name: lastName,
+		    password: password,
+		    password_confirmation: passwordConf
+		}, registerCallback);
+	function registerCallback(e) {
+		console.log(util.inspect(e));
+		var response = {status: "failure"};
+		if (e.success) {
+			response = {status: "success"};
+		}
+		res.send(response);
+	}
+};
+
+api.register = function(req, res){
+	res.render('register', {
+		message : 'Register',
+		layout: false
+	 });
+};
+
+api.index = function(req, res){
+	res.render('index', {
+		message : 'Login',
+		layout: false
+	 });
+};
+
+api.home = function(req, res){
+	res.render('home', {
+		message : 'Home',
+		layout: false
+	 });
+};
+
+api.createPlace = function(req, res) {
+    ACS.Places.create({ 
+        name: 'nameOfCity33', 
+        city: 'Some City2332', 
+        session_id: sessionId
+    }, function(data) {
+        res.text('New place created!');
+    });
+};
